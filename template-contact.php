a70b9d0315c31e71421541447e85ab272db3bd74
Michael Nix <mpnix@protonmail.com>
2021-03-02 12:44:33 +0000

    Improved layout of contact form

--- template-contact.php
+++ template-contact.php
@@ -9,22 +9,24 @@ get_header();
 <div class="container">
     <div class="contact">
         <div>
-        <div>
             <form action="/" method="post">
-                <label for="name">Name</label>
-                <input type="text" id="name"/>
-                <label for="email">Your email</label>
-                <input type="text" id="email"/>
-                <label for="message">Message</label>
-                <textarea id="message"></textarea>
-                <button type="submit">Send</button>
+                <div>
+                    <label for="name">Name</label>
+                    <input type="text" id="name"/>
+                    <label for="email">Your email</label>
+                    <input type="text" id="email"/>
+                </div>
+                <div>
+                    <label for="message">Message</label>
+                    <textarea id="message"></textarea>
+                    <button type="submit">Send</button>
+                </div>
             </form>
         </div>
-        </div>
         <div>
-        <div class="highlight-area">
-            <?php the_content(); ?>
-        </div>
+            <div class="highlight-area">
+                <?php the_content(); ?>
+            </div>
         </div>
     </div>
 </div>
